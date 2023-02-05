1c24bc640fe31321dd17c544c16b9b30ef1bc142
Sander Vonk <sander.c.vonk@gmail.com>
2023-02-05 01:20:21 -0800

    Updates

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/focustime-vue/" : "/",
-  configureWebpack: {
-    plugins: [new MiniCssExtractPlugin()],
-  },
-};
